056011f28a169c54efea116d1ef5a8c81313f687
simran61 <simrangangwani3107@gmail.com>
2024-08-11 03:39:15 +0530

    update: added show more button

--- src/components/BestCuisines.js
+++ src/components/BestCuisines.js
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const BestCuisines = ({ cuisines }) => {
   const { brands, title } = cuisines;
+  const [visible, setVisible] = useState(11);
+  const [showBtn, setShowBtn] = useState(true);
+
+  const showMoreItems = () => {
+    setVisible((prevValue) => prevValue + (brands.length - visible));
+    setShowBtn(false);
+  };
+
   return (
-    <div>
+    <div className="my-8">
       <h1 className="font-bold text-xl py-3">{title}</h1>
       <div className="grid grid-cols-4">
-        {brands.map((city) => (
+        {brands.slice(0, visible).map((city) => (
           <Link to={city.link}>
             <button className="border rounded-lg py-3 my-2 w-[270px] font-semibold">
               {city.text}
             </button>
           </Link>
         ))}
+        {showBtn && (
+          <button
+            className="flex items-center justify-center border rounded-lg py-3 my-2 w-[270px] font-semibold"
+            onClick={showMoreItems}
+          >
+            <span className="font-bold">Show More</span>
+            <img
+              className="h-5 ml-2"
+              src="https://static.thenounproject.com/png/1123247-200.png"
+              alt=""
+            />
+          </button>
+        )}
       </div>
     </div>
   );
