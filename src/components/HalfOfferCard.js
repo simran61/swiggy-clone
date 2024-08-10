1f3fba40c16f7343142cfb33883410ebf0a8a79c
simran61 <simrangangwani3107@gmail.com>
2024-08-10 15:50:59 +0530

    style: resMenu header

--- src/components/HalfOfferCard.js
+++ src/components/HalfOfferCard.js
@@ -2,15 +2,17 @@ import React from "react";
 
 const HalfOfferCard = ({ data }) => {
   return (
-    <div className="border rounded-2xl p-2 flex items-center">
+    <div className="border rounded-2xl p-3 mr-4 my-4 flex items-center">
       <img
         className="w-12 mx-2"
         src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_96,h_96/offers/generic"
         alt=""
       />
-      <div>
+      <div className="ml-1">
         <h3 className="font-extrabold">{data?.info?.header}</h3>
-        <span className="text-sm">{data?.info?.couponCode}</span>
+        <span className="text-sm font-bold text-[#02060c72]">
+          {data?.info?.couponCode}
+        </span>
       </div>
     </div>
   );
--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -40,7 +40,6 @@ const RestaurantMenu = () => {
         c.card?.card?.["@type"] ===
         "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
     );
-  console.log(categories);
 
   const nestedCategories =
     resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
@@ -48,7 +47,6 @@ const RestaurantMenu = () => {
         nc.card?.card?.["@type"] ===
         "type.googleapis.com/swiggy.presentation.food.v2.NestedItemCategory"
     );
-  console.log(nestedCategories);
 
   const topPicks =
     resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
@@ -63,90 +61,114 @@ const RestaurantMenu = () => {
     dots: false,
     infinite: false,
     speed: 500,
+    slidesToShow: 2.26,
+    slidesToScroll: 1,
+  };
+
+  var settings1 = {
+    dots: false,
+    infinite: false,
+    speed: 500,
     slidesToShow: 2.4,
     slidesToScroll: 1,
   };
 
   return (
     <div className="flex flex-col w-1/2 my-8 mx-auto">
-      <span>
-        Home/{city}/{name}
-      </span>
+      <div className="text-[10px]">
+        <span className="text-[#93959f]">Home / {city} / </span>
+        <span className="text-[#535665]">{name}</span>
+      </div>
 
-      <h1 className="font-bold my-6 text-2xl">{name}</h1>
+      <h1 className="font-extrabold mt-6 mb-4 text-2xl">{name}</h1>
 
-      <div className="border rounded-lg p-4 m-4">
-        <div className="flex">
-          <p className="flex items-center">
-            <svg
-              width="20"
-              height="20"
-              viewBox="0 0 20 20"
-              fill="none"
-              role="img"
-              aria-hidden="true"
-              strokeColor="rgba(2, 6, 12, 0.92)"
-              fillColor="rgba(2, 6, 12, 0.92)"
-            >
-              <circle
-                cx="10"
-                cy="10"
-                r="9"
-                fill="url(#StoreRating20_svg__paint0_linear_32982_71567)"
-              ></circle>
-              <path
-                d="M10.0816 12.865C10.0312 12.8353 9.96876 12.8353 9.91839 12.865L7.31647 14.3968C6.93482 14.6214 6.47106 14.2757 6.57745 13.8458L7.27568 11.0245C7.29055 10.9644 7.26965 10.9012 7.22195 10.8618L4.95521 8.99028C4.60833 8.70388 4.78653 8.14085 5.23502 8.10619L8.23448 7.87442C8.29403 7.86982 8.34612 7.83261 8.36979 7.77777L9.54092 5.06385C9.71462 4.66132 10.2854 4.66132 10.4591 5.06385L11.6302 7.77777C11.6539 7.83261 11.706 7.86982 11.7655 7.87442L14.765 8.10619C15.2135 8.14085 15.3917 8.70388 15.0448 8.99028L12.7781 10.8618C12.7303 10.9012 12.7095 10.9644 12.7243 11.0245L13.4225 13.8458C13.5289 14.2757 13.0652 14.6214 12.6835 14.3968L10.0816 12.865Z"
-                fill="white"
-              ></path>
-              <defs>
-                <linearGradient
-                  id="StoreRating20_svg__paint0_linear_32982_71567"
-                  x1="10"
-                  y1="1"
-                  x2="10"
-                  y2="19"
-                  gradientUnits="userSpaceOnUse"
-                >
-                  <stop stop-color="#21973B"></stop>
-                  <stop offset="1" stop-color="#128540"></stop>
-                </linearGradient>
-              </defs>
-            </svg>
-            <span>{avgRating}</span>
-            <span>({totalRatingsString})</span>
-          </p>
-          <span>•</span>
-          <p>{costForTwoMessage}</p>
-        </div>
-        <span>{cuisines.join(", ")}</span>
-        <div className="pl-2 border border-l-2 border-t-0 border-r-0 border-b-0">
-          <div>
-            <span className="mr-2">Outlet</span>
-            <span>{areaName}</span>
+      <div className="res-info-box-bg">
+        <div className="bg-white p-4 mt-2 res-info-box rou">
+          <div className="flex">
+            <p className="flex items-center font-bold">
+              <svg
+                width="18"
+                height="18"
+                viewBox="0 0 20 20"
+                fill="none"
+                role="img"
+                aria-hidden="true"
+                strokeColor="rgba(2, 6, 12, 0.92)"
+                fillColor="rgba(2, 6, 12, 0.92)"
+              >
+                <circle
+                  cx="10"
+                  cy="10"
+                  r="9"
+                  fill="url(#StoreRating20_svg__paint0_linear_32982_71567)"
+                ></circle>
+                <path
+                  d="M10.0816 12.865C10.0312 12.8353 9.96876 12.8353 9.91839 12.865L7.31647 14.3968C6.93482 14.6214 6.47106 14.2757 6.57745 13.8458L7.27568 11.0245C7.29055 10.9644 7.26965 10.9012 7.22195 10.8618L4.95521 8.99028C4.60833 8.70388 4.78653 8.14085 5.23502 8.10619L8.23448 7.87442C8.29403 7.86982 8.34612 7.83261 8.36979 7.77777L9.54092 5.06385C9.71462 4.66132 10.2854 4.66132 10.4591 5.06385L11.6302 7.77777C11.6539 7.83261 11.706 7.86982 11.7655 7.87442L14.765 8.10619C15.2135 8.14085 15.3917 8.70388 15.0448 8.99028L12.7781 10.8618C12.7303 10.9012 12.7095 10.9644 12.7243 11.0245L13.4225 13.8458C13.5289 14.2757 13.0652 14.6214 12.6835 14.3968L10.0816 12.865Z"
+                  fill="white"
+                ></path>
+                <defs>
+                  <linearGradient
+                    id="StoreRating20_svg__paint0_linear_32982_71567"
+                    x1="10"
+                    y1="1"
+                    x2="10"
+                    y2="19"
+                    gradientUnits="userSpaceOnUse"
+                  >
+                    <stop stop-color="#21973B"></stop>
+                    <stop offset="1" stop-color="#128540"></stop>
+                  </linearGradient>
+                </defs>
+              </svg>
+              <span className="mx-1">{avgRating}</span>
+              <span>({totalRatingsString})</span>
+            </p>
+            <span className="text-[#02060c72] mx-2">•</span>
+            <p className="font-bold">{costForTwoMessage}</p>
           </div>
-          <span>{sla.slaString.toLowerCase()}</span>
-        </div>
-        <hr className="my-3" />
-        <div className="flex items-center">
-          <img
-            className="h-5"
-            src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_40,h_40/v1648635511/Delivery_fee_new_cjxumu"
-            alt="delivery"
-          />
-          <span>{sla?.lastMileTravelString} | </span>
-          <span>₹{feeDetails?.amount / 100} Delivery fee will apply</span>
+          <span className="text-[#ff5200] font-bold underline text-sm">
+            {cuisines.join(", ")}
+          </span>
+          <div className="flex my-3 w-full text-sm">
+            <div class="sc-fmzyuX jICcAR mr-2">
+              <div class="sc-cyRcrZ cAzFIF"></div>
+              <div class="sc-kUdmhA gzkjpM"></div>
+              <div class="sc-cyRcrZ cAzFIF"></div>
+            </div>
+            <div>
+              <p>
+                <span className="font-bold mr-2">Outlet </span>{" "}
+                <span className="text-[#02060c99]">{areaName}</span>
+              </p>
+              <span className="font-bold">{sla.slaString.toLowerCase()}</span>
+            </div>
+          </div>
+          <hr className="my-3" />
+          <div className="flex items-center text-[#02060c99] text-sm font-medium">
+            <img
+              className="h-5"
+              src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_40,h_40/v1648635511/Delivery_fee_new_cjxumu"
+              alt="delivery"
+            />
+            <span className="ml-2">{sla?.lastMileTravelString} |</span>
+            <span className="ml-1">
+              ₹{feeDetails?.amount / 100} Delivery fee will apply
+            </span>
+          </div>
         </div>
       </div>
 
-      <div>
-        <h1>Deals for you</h1>
+      {offers && (
+        <div className="my-8">
+          <h1 className="text-xl font-extrabold">Deals for you</h1>
 
-        <Slider {...settings}>
-          {offers.map((offer) => (
-            <HalfOfferCard data={offer} />
-          ))}
-        </Slider>
-      </div>
+          <Slider {...settings}>
+            {offers.map((offer) => (
+              <HalfOfferCard data={offer} />
+            ))}
+          </Slider>
+        </div>
+      )}
 
       <div className="my-8">
         <div className="flex items-center justify-center">
@@ -206,7 +228,7 @@ const RestaurantMenu = () => {
 
         <div>
           <h1 className="font-bold text-xl">Top Picks</h1>
-          <Slider {...settings}>
+          <Slider {...settings1}>
             {topBanners &&
               topBanners.map((banner) => <TopPicks topCard={banner} />)}
           </Slider>
